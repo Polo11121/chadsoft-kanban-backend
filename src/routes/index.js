36f76019d074bfcdd1cc022b470fa4ad5ddd47bb
unknown <159154@student.uwm.edu.pl>
2022-03-29 22:57:21 +0200

    Added numberOfTask

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import cookieParser from 'cookie-parser';
 import { Router } from 'express';
 
 import { arrayColumnsRouter } from './arrayColumns.routes';
-// import { userRoutes } from '../Logowanie/user.routes';
 import { columnRouter } from './column.routes';
 import { memberRouter } from './members.routes';
 import { sectionRouter } from './section.routes';
@@ -17,5 +16,4 @@ columnRouter(router);
 sectionRouter(router);
 memberRouter(router);
 arrayColumnsRouter(router);
-// userRoutes(router);
 export default router;
